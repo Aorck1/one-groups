f11634fcfdb25fa9f603f356c209fbbaf219a6c8
Aorck1 <26223446422@qq.com>
2023-05-29 17:04:30 +0800

    “这是添加ElementUI”

--- System/src/main.js
+++ System/src/main.js
@@ -1,12 +1,22 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import 'element-plus/theme-chalk/index.css';
+
+
+
+
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+
+
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App)
+app.use(ElementPlus)
 
 app.use(createPinia())
 app.use(router)
--- System/src/views/AboutView.vue
+++ System/src/views/AboutView.vue
@@ -4,6 +4,7 @@
       哈哈哈哈
       <div>hhhh</div>
       <button @click="test">test</button>
+      <el-button>Default</el-button>
     </h1>
   </div>
 </template>
